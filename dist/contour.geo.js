6224fabc4f79775e1161b63b2435716d0209ab26
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-06-12 10:42:30 -0700

    Release 0.1.4

--- dist/contour.geo.js
+++ dist/contour.geo.js
@@ -94,10 +94,9 @@
             .projection(projection);
 
         var getCentroid = function (element) {
-            var parentBox = element.offsetParent.getBoundingClientRect();
             var bbox = element.getBoundingClientRect();
 
-            return [bbox.left - parentBox.left + bbox.width/2, bbox.top - parentBox.top + bbox.height/2];
+            return [bbox.left + bbox.width/2, bbox.top + bbox.height/2];
         };
 
         var map = this.svg.select('#states');
@@ -148,4 +147,4 @@
     });
 })();
 
-Contour.geo.version = '0.1.3';
+Contour.geo.version = '0.1.4';
--- src/scripts/version.js
+++ src/scripts/version.js
@@ -1,1 +1,1 @@
-Contour.geo.version = '0.1.3';
+Contour.geo.version = '0.1.4';
