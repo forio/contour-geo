7cd01d7b128c6cae228c107d01856927250cab9c
Molly Jones <mjones@forio.com>
2014-10-15 09:44:52 -0700

    infrastructure for building docs
    update build-documentation.js
    copy config-dox.js from regular contour

--- build-documentation.js
+++ build-documentation.js
@@ -1,6 +1,7 @@
 
 var glob = require('glob');
 var fs = require('fs');
+var confdox = require('./config-dox');
 var markdox = require('markdox');
 var async = require('async');
 var path = require('path');
@@ -9,7 +10,11 @@ var _ = require('lodash');
 var doxTemplate = __dirname + '/src/documentation/file-dox-template.ejs';
 var docFolder = __dirname + '/documentation/';
 var configDocFolder = docFolder + 'config/';
+
 var docSrcFolder = __dirname + '/src/documentation/';
+var configTemplateDocFolder = docSrcFolder + 'config/';
+
+var propTemplatePath = docSrcFolder + 'prop-template.md';
 
 var options = {
     src: [
@@ -20,12 +25,13 @@ var options = {
 };
 
 var allFiles = getSourceFileList(options.src);
-var filesToCopy = ['connectors.md'];
+var filesToCopy = ['contour-geo.md', 'geo-included.md', 'topojson.md'];
 
 ensureDirectory([docFolder]);
 
 generatePerFileDoc(allFiles);
 generateAllFilesDoc(allFiles);
+generateConfigObjectDoc(allFiles);
 
 copyDocViewer(filesToCopy);
 
@@ -123,3 +129,91 @@ function generateAllFilesDoc(allFiles) {
     });
 }
 
+
+function generateConfigObjectDoc(files) {
+    var configObject = {};
+    files.forEach(function (file) {
+        var js = fs.readFileSync(file).toString();
+        var res = confdox.parseConfigObject(js);
+
+        // the order of the files is from generic to specific
+        // and we want to document through the global config file
+        // the most generic version of the config property
+        configObject = _.merge({}, res, configObject);
+    });
+
+    fs.readFile(propTemplatePath, 'utf8', function (err, propTemplate) {
+
+        var menu = {};
+        var namespace = function (obj, path) {
+            var parts = path.split('.');
+            while(parts.length) {
+                var sub = parts.shift();
+                if(!obj[sub]) obj[sub] = {};
+                obj = obj[sub];
+            }
+
+            return obj;
+        };
+
+        // we want to traverse the config file
+        bfs(configObject, 'config', function (k) {
+            var propId = (k.ctx ? k.ctx + '.' + k.key : k.key);
+            var fileName = propId + '.md';
+            var fullPath = configDocFolder + fileName;
+            var templateFileName = configTemplateDocFolder + (k.ctx ? k.ctx + '.' + k.key : k.key) + '.md';
+            var template;
+            var fiddleLink = 'http://jsfiddle.net/gh/get/jquery/1.7.2/forio/contour-geo/tree/master/src/documentation/fiddle/' + propId + '/';
+
+            if(fs.existsSync(templateFileName)) {
+                template = fs.readFileSync(templateFileName, 'utf8');
+            } else {
+                var compiled = _.template(propTemplate);
+                template = compiled({
+                    name: k.key,
+                    type: '<%= type %>',
+                    notes: '<% if(notes) { %><%= notes %><% } %>',
+                    defaultValue: '<% if(defaultValue !== "[object Object]") { %>*default: <%= defaultValue %>* <% }%>'
+                });
+
+                fs.writeFileSync(templateFileName, template, 'utf8');
+            }
+
+            // bake and write out the template into an .md file
+            var cooked = _.template(template)({
+                type: typeof k.ref,
+                notes: '',
+                defaultValue: k.ref === null ? 'null' : k.ref === undefined ? 'undefined' : k.ref.toString(),
+                jsFiddleLink: fiddleLink
+            });
+
+            fs.writeFile(fullPath, cooked, 'utf8');
+
+            // update the menu object
+            //namespace(menu, [k.ctx, k.key].join('.')).path = fileName;
+        });
+
+        // write out the menu object
+        //fs.writeFileSync(configDocFolder + 'menu.json', JSON.stringify(menu), 'utf8');
+
+    });
+}
+
+function bfs(root, rootContext, visitor) {
+    var queue = [];
+
+    queue.push({key: rootContext, ref: root, ctx: '' });
+
+    while(queue.length) {
+        var cur = queue.shift();
+        var parent = typeof cur.ref === 'object';
+        visitor(cur, parent);
+        // console.log('testing ' + cur.key + ' -> ' + typeof cur.ref);
+        if (parent) {
+            for(var key in cur.ref) {
+                context = cur.ctx ? cur.ctx + '.' + cur.key : cur.key;
+                queue.push({key: key, ref: cur.ref[key], ctx: context });
+            }
+        }
+    }
+}
