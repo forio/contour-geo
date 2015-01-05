69d919c202164fb822f95844ad9bd1965166595e
Molly Jones <mjones@forio.com>
2015-01-05 15:23:14 -0800

    us-map: ensureDefaults from map; add docs

--- src/scripts/us-map.js
+++ src/scripts/us-map.js
@@ -2,7 +2,7 @@
     'use strict';
 
     var renderer = function (data, layer, options) {
-        this.ensureDefaults(options, 'choropleth');
+        this.ensureDefaults(options, 'map');
 
         options.choropleth = options.choropleth || {};
         _.merge(options.choropleth, {
@@ -12,6 +12,22 @@
 
         return this.choropleth.renderer.call(this, data, layer, options);
     };
+
+    /**
+    * Adds a map visualization to the Contour instance, using the `albersUsa` projection and a TopoJSON file with data on US states, such as the `us.json` and `us-all.json` TopoJSON files included with Contour-Geo. This visualization is a shorthand for configuring a `.map()` visualization that is focused on the US.
+    *
+    * ### Example:
+    *
+    *       d3.json('us-all.json', function (us) {
+    *           new Contour({ el: '.map' })
+    *               .USMap(us)
+    *               .render()
+    *       });
+    *
+    * @name USMap(data, options)
+    * @param {object} data The data (topology) to be rendered with this visualization. This must be in TopoJSON format.
+    * @param {object} options (Optional) Configuration options particular to this visualization that override the defaults.
+    */
 
     Contour.export('USMap', renderer);
 })();
