0493dd624603d5562d4c6e5e3d4ffb7b09939eb7
Jaime del Palacio <jaimedelpalacio@forio.com>
2015-07-15 14:56:01 -0700

    updated version file to 0.9.117

--- dist/contour.geo.js
+++ dist/contour.geo.js
@@ -1,4 +1,4 @@
-/*! Contour-Geo - v0.9.116 - 2015-07-15 */
+/*! Contour-Geo - v0.9.117 - 2015-07-15 */
 
 (function(exports, global) {
     global["true"] = exports;
@@ -316,7 +316,7 @@
     */
         Contour.export("USMap", renderer);
     })();
-    Contour.geo.version = "0.9.116";
+    Contour.geo.version = "0.9.117";
     (function() {
         "use strict";
         var renderer = function(data, layer, options) {
--- src/scripts/version.js
+++ src/scripts/version.js
@@ -1,1 +1,1 @@
-Contour.geo.version = '0.9.116';
+Contour.geo.version = '0.9.117';
